baeb8c56660bcc06ea878a4c63b844e37aa450e6
Milad Karami <miladkarami990830@gmail.com>
2022-01-03 16:25:43 +0330

    commit

--- backend/urls.py
+++ backend/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
+from django.urls import path,include
+import index.urls as index_urls
+urlpatterns=[
+  path('admin/',admin.site.urls),
+  path('',include('index.urls')),
 ]
